6f6027c0f06525c939008baee2049e5ee9de1c67
DurgaPrasad Sammengi <durgaprasad_sammengi@persistent.com>
2022-07-23 00:23:56 +0530

    first commit

---
+++ src/main/java/com/main/rest/EmpService.java
@@ -0,0 +1,12 @@
+package com.main.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmpService {
+    @GetMapping("/message")
+    public String getEmp(){
+        return "emp Prasd";
+    }
+}
---
+++ src/main/java/com/main/rest/RestServApplication.java
@@ -0,0 +1,13 @@
+package com.main.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServApplication.class, args);
+	}
+
+}
